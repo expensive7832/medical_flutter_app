2ad7de6d60218025b9ef5f73180f6b54bc6bb45d
jimoh yusu <expensive7832@gmail.com>
2023-07-30 13:41:26 +0100

    just testing

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -62,7 +62,7 @@ class _HomeState extends State<Home> {
                     child: TextField(
                       autocorrect: true,
                       decoration: InputDecoration(
-                        hintText: "Search here...",
+                        hintText: "Search here.",
                         border: InputBorder.none
                       ),
                     ),
